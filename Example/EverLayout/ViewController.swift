66f7249f7e70116a409392b443f37696c2c2e0d7
Dale Webster <dalewebster48@gmail.com>
2017-05-03 10:20:36 +0100

    Rearranged the entire proejct so it better resembles a CocoaPods project

---
+++ Example/EverLayout/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  EverLayout
+//
+//  Created by acrocat on 05/03/2017.
+//  Copyright (c) 2017 acrocat. All rights reserved.
+//
+
+import UIKit
+import EverLayout
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
